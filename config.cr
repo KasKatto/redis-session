3e5b66efcdc789315e2964783de49771e0abf696
KasKatto <48245049+KasKatto@users.noreply.github.com>
2021-05-10 12:54:12 +0100

    Update config.cr
    fixed config notation

--- config.cr
+++ config.cr
@@ -1,12 +1,12 @@
 RedisSession.configure do |settings|
   # Sets the name for the session id cookie
-  setting session_id_name : String = "_session"
+  settings.session_id_name = "_session"
   # Sets the id length
-  setting session_id_length : Int32 = 32
+  settings.session_id_length = 32
   # Should session storage be encrypted? (redis does not support encryption at rest - unless your using a cloud service like Redis Cloud or AWS)
-  setting encrpyt_session_storage : Bool = false
+  settings.encrpyt_session_storage = false
   # Sets how long a session will idle before being removed (idle timer reset every time session is written or read)
-  setting session_idle_time : Time::Span = 60.minutes
+  settings.session_idle_time = 60.minutes
   # URI to redis server -
-  setting redis_uri : String = "redis://localhost:6379"
+  settings.redis_uri = "redis://localhost:6379/0"
 end
